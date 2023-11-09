bb225b0ccd40ec815c5664a455c31ca4d8bacdc1
ines <ines.magessi@tecnico.ulisboa.pt>
2023-11-09 15:42:24 +0000

    added polynomial complexity tracking

--- src/run.py
+++ src/run.py
@@ -3,7 +3,7 @@ import pandas as pd
 from tqdm import tqdm
 
 from data import read_dataset
-from tiny_gp import evolve
+from tiny_gp_parallel import evolve
 from configs import GENERATIONS
 
 #####################################################
@@ -25,6 +25,7 @@ def run_stdGP(ds_name):
     train_best_ind = pd.DataFrame(columns = [i for i in range(0, GENERATIONS + 1)])
     best_of_run_generations = pd.DataFrame(columns = ['Gen_Number'])
     iodc_results = pd.DataFrame(columns = [i for i in range(0, GENERATIONS + 1)])
+    p_analysis_results = pd.DataFrame(columns = [i for i in range(0, GENERATIONS + 1)])
 
     # Run for 30 times with each dataset partition
     for run_nr in tqdm(range(1, 31)):
@@ -35,26 +36,32 @@ def run_stdGP(ds_name):
         terminals = [f'x{i}' for i in range(1, len(train_dataset[0]) + 1)]
 
         # Run GP
-        best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen, iodc_list = \
+        best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen, iodc_list, p_analysis_list = \
             evolve(train_dataset, test_dataset, train_target, test_target, terminals)
+
+        # best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen, = \
+        #     evolve(train_dataset, test_dataset, train_target, test_target, terminals)
         
         train_fit = pd.DataFrame([best_train_fit_list], columns = [i for i in range(0, GENERATIONS + 1)])
         test_fit = pd.DataFrame([best_test_fit_list], columns = [i for i in range(0, GENERATIONS + 1)])
         best_ind = pd.DataFrame([best_ind_list], columns = [i for i in range(0, GENERATIONS + 1)])
         best_gen = pd.DataFrame([best_of_run_gen], columns = ['Gen_Number'])
         iodc = pd.DataFrame([iodc_list], columns = [i for i in range(0, GENERATIONS + 1)])
-
+        p_analysis = pd.DataFrame([p_analysis_list], columns = [i for i in range(0, GENERATIONS + 1)])
+        
         train_results = pd.concat([train_results, train_fit], ignore_index = True)
         test_results = pd.concat([test_results, test_fit], ignore_index = True)
         train_best_ind = pd.concat([train_best_ind, best_ind], ignore_index = True)
         best_of_run_generations = pd.concat([best_of_run_generations, best_gen], ignore_index = True)
         iodc_results = pd.concat([iodc_results, iodc], ignore_index = True)
+        p_analysis_results = pd.concat([p_analysis_results, p_analysis], ignore_index = True)
 
     train_results.to_csv(SAVE_PATH + 'train.csv')
     test_results.to_csv(SAVE_PATH + 'test.csv')
     train_best_ind.to_csv(SAVE_PATH + 'best_ind.csv')
     best_of_run_generations.to_csv(SAVE_PATH + 'best_gen.csv')
     iodc_results.to_csv(SAVE_PATH + 'iodc_complexity.csv')
+    p_analysis_results.to_csv(SAVE_PATH + 'polynomial_complexity.csv')
         
 
 def run_StdGP_all_ds():
@@ -67,6 +74,6 @@ def run_StdGP_all_ds():
         run_stdGP(dataset)
 
 
-run_stdGP('Concrete')
+# run_stdGP('Concrete')
 
-# run_StdGP_all_ds()
+run_StdGP_all_ds()
