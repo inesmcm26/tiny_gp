4bc87c4e08243cbd183c07c73679bf66f5281eb5
ines <ines.magessi@tecnico.ulisboa.pt>
2023-12-22 11:44:43 +0000

    added xo and mutation max depth exceeded. added pop size exceed remove worst ind. saving sizes

--- src/tiny_gp.py
+++ src/tiny_gp.py
@@ -1,4 +1,4 @@
-from random import randint, random
+from random import randint, random, choice
 from copy import deepcopy
 import numpy as np
 import time
@@ -108,7 +108,7 @@ def evolve(train_dataset, test_dataset, train_target, test_target, terminals):
 
     # Upper bounds for complexities
     z, max_IODC = init_IODC(train_dataset, train_target)
-    # max_slope_complexity = init_slope_based_complexity(train_dataset, train_target)
+    max_slope_complexity = init_slope_based_complexity(train_dataset, train_target)
 
     train_fitnesses = [fitness(ind, train_dataset, train_target) for ind in population]
     test_fitnesses = [fitness(ind, test_dataset, test_target) for ind in population]
@@ -127,18 +127,20 @@ def evolve(train_dataset, test_dataset, train_target, test_target, terminals):
     mean_test_fit_list = [np.mean(test_fitnesses)]
     # Save complexities
     iodc = [IODC(max_IODC, z, best_of_run, train_dataset)]
-    # p_analysis = [polynomial_analysis(best_of_run)]
-    # slope = [slope_based_complexity(max_slope_complexity, best_of_run, train_dataset)]
-    # Save mean complexities
+    slope = [slope_based_complexity(max_slope_complexity, best_of_run, train_dataset)]
+    # Save complexity distributions
     iodc_distribution = [[IODC(max_IODC, z, ind, train_dataset) for ind in population]]
+    slope_distribution = [[slope_based_complexity(max_slope_complexity, ind, train_dataset) for ind in population]]
+    # Save mean complexities
     mean_iodc = [np.mean(iodc_distribution[0])]
-    # mean_p_analysis = [mean_polynomial_analysis(population)]
-    # mean_slope = [mean_slope_based_complexity(max_slope_complexity, population, train_dataset)]
-    # Save complexity distribution
-    # Save overfitting
-    overfit = [0]
-    btp = best_test_fit_list[0]
-    tbtp = best_of_run_f
+    mean_slope = [np.mean(slope_distribution[0])]
+    # Save best ind size
+    size = [best_of_run.size()]
+    # Save sizes
+    size_distribution = [[ind.size() for ind in population]]
+    # Save mean sizes
+    mean_size = [np.mean(size_distribution[0])]
+    # Save measure of interpretability
 
     for gen in range(1, GENERATIONS + 1):  
         # print('------------------------------------------ NEW GEN ------------------------------------------')
@@ -159,15 +161,23 @@ def evolve(train_dataset, test_dataset, train_target, test_target, terminals):
                 # print('CROSSOVER')
                 parent2 = tournament(population, train_fitnesses)
 
+                parent_orig = deepcopy(parent)
+                parent2_orig = deepcopy(parent)
+
                 parent.crossover(parent2)
+
+                # If children exceed parents
+                if parent.depth() > MAX_DEPTH:
+                    parent = choice([parent_orig, parent2_orig])
+                
+                if parent2.depth() > MAX_DEPTH:
+                    parent2 = choice([parent_orig, parent2_orig])
 
                 if parent.depth() > MAX_DEPTH or parent2.depth() > MAX_DEPTH:
                     raise Exception('Crossover generated an individual that exceeds depth.')
                 
                 new_pop.append(parent)
-
-                if len(new_pop) < POP_SIZE:
-                    new_pop.append(parent2)
+                new_pop.append(parent2)
 
             # Mutation
             elif prob < XO_RATE + PROB_MUTATION:
@@ -182,55 +192,28 @@ def evolve(train_dataset, test_dataset, train_target, test_target, terminals):
             # NOTE: Replication may also occur if no condition is met
             else:
                 new_pop.append(parent)
-            
-        population = new_pop
 
-        print('NEW POP DONE', time.time() - start)
+        new_train_fitnesses = [fitness(ind, train_dataset, train_target) for ind in new_pop]
 
-        start = time.time()
-
-        train_fitnesses = [fitness(ind, train_dataset, train_target) for ind in population]
-        print('NEW TRAIN FITNESSES DONE', time.time() - start)
+        # Check if len population exceeded
+        if len(new_pop) > POP_SIZE:
+            print('POP SIZE EXCEEDED')
+            # Remove worst individual
+            idx_worst = new_train_fitnesses.index(max(new_train_fitnesses))
+            new_pop.pop(idx_worst)
+            new_train_fitnesses.pop(idx_worst)
         
-        start = time.time()
+        if len(new_pop) != POP_SIZE:
+            raise Exception('POP SIZE EXCEEDED!!!')
+            
+        population = new_pop.copy()
+        train_fitnesses = new_train_fitnesses.copy()
         test_fitnesses = [fitness(ind, test_dataset, test_target) for ind in population]
-        print('NEW TEST FITNESSES DONE', time.time() - start)
-
-        # # You can set the number of processes as desired
-        # num_processes = 8
-
-        # pool = Pool(processes=num_processes)
-
-        # train_fitnesses = pool.starmap(fitness, [(ind, train_dataset, train_target) for ind in population])
-
-        # pool.close()
-        # pool.join()
         
         if min(train_fitnesses) < best_of_run_f:
             best_of_run_f = min(train_fitnesses)
             best_of_run_gen = gen
             best_of_run = deepcopy(population[train_fitnesses.index(min(train_fitnesses))])        
-
-        # ---------------------------------- Overfit ---------------------------------- #
-        # # TODO: Confirm!
-        # # Performance of the individual with the best train fitness in this generation
-        # test_performance = fitness(deepcopy(population[train_fitnesses.index(min(train_fitnesses))]), test_dataset, test_target)
-
-        # if min(train_fitnesses) > test_performance:
-        #     overfit.append(0)
-        # else:
-        #     if test_performance < btp:
-        #         btp = test_performance
-        #         overfit.append(0)
-        #         tbtp = min(train_fitnesses)
-        #     else:
-        #         overfit.append(abs(min(train_fitnesses) - test_performance) - abs(tbtp - btp))
-            
-        # ---------------------------------------------------------------------------- #
-           
-        # print("________________________")
-        # print("gen:", gen, ", best_of_run_f:", round(min(train_fitnesses), 3), ", best_of_run:") 
-        # best_of_run.print_tree()
         
         # Save best train performance
         best_train_fit_list.append(best_of_run_f)
@@ -244,25 +227,28 @@ def evolve(train_dataset, test_dataset, train_target, test_target, terminals):
 
         # Save complexities
         iodc.append(IODC(max_IODC, z, best_of_run, train_dataset))
-        # p_analysis.append(polynomial_analysis(best_of_run))
-        # slope.append(slope_based_complexity(max_slope_complexity, best_of_run, train_dataset))
-        # print('BEST COMPLEXITIES DONE', time.time() - start)
+        slope.append(slope_based_complexity(max_slope_complexity, best_of_run, train_dataset))
+
+        # Save complexity distributions
+        iodc_distribution.append([IODC(max_IODC, z, ind, train_dataset) for ind in population])
+        slope_distribution.append([slope_based_complexity(max_slope_complexity, ind, train_dataset) for ind in population])
 
         # Save mean complexities
-        iodc_distribution.append([IODC(max_IODC, z, ind, train_dataset) for ind in population])
         mean_iodc.append(np.mean(iodc_distribution[-1]))
+        mean_slope.append(np.mean(slope_distribution[-1]))
 
-        # start = time.time()
-        # mean_p_analysis.append(mean_polynomial_analysis(population))
-        # print('MEAN POLYNOMIAL DONE', time.time() - start)
+        # Save size
+        size.append(best_of_run.size())
 
-        # start = time.time()
-        # mean_slope.append(mean_slope_based_complexity(max_slope_complexity, population, train_dataset))
-        # print('MEAN SLOPE DONE', time.time() - start)
+        # Save size distribution
+        size_distribution.append([ind.size() for ind in population])
+
+        # Save mean size
+        mean_size.append(np.mean(size_distribution[-1]))
 
         print('NEW BEST FINTESS', best_of_run_f)
         print('FITNESS IN TEST', best_test_fit_list[-1])
-        print('BEST IND IODC COMPLEXITY', iodc[-1])
+        
         # print('IODC DISTRIBUTION', iodc_distribution[-1])
 
         # Optimal solution found
@@ -271,10 +257,12 @@ def evolve(train_dataset, test_dataset, train_target, test_target, terminals):
     
     print("\n\n_________________________________________________\nEND OF RUN\nbest_of_run attained at gen " + str(best_of_run_gen) +\
           " and has f=" + str(round(best_of_run_f, 3)))
-    # best_of_run.print_tree()
 
-    return best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen, mean_train_fit_list, mean_test_fit_list, \
-        iodc, mean_iodc, iodc_distribution
+    return best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen, \
+        mean_train_fit_list, mean_test_fit_list, \
+        iodc, mean_iodc, iodc_distribution, \
+        slope, mean_slope, slope_distribution, \
+        size, mean_size, size_distribution
     
 # if __name__== "__main__":
 #   best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen = evolve()
