4aa2ca9f09ab17c6cf1306051e86b42f77904e4c
ines <ines.magessi@tecnico.ulisboa.pt>
2024-01-04 21:44:32 +0000

    saving complexity in augmented dataset

--- src/run.py
+++ src/run.py
@@ -21,22 +21,22 @@ def run_stdGP(ds_name):
         os.makedirs(SAVE_PATH)
     
     # Run for 30 times with each dataset partition
-    for run_nr in tqdm(range(1, 31)): # TODO: CHANGE HERE!
+    for run_nr in tqdm(range(1, 10)): # TODO: CHANGE HERE!
         
         # Get correct data partition
-        train_dataset, test_dataset, train_target, test_target = read_dataset(ds_name, run_nr)
+        train_dataset, test_dataset, augmented_dataset, train_target, test_target = read_dataset(ds_name, run_nr)
 
         terminals = [f'x{i}' for i in range(1, len(train_dataset[0]) + 1)]
 
         # Run GP
         best_train_fit_list, best_test_fit_list, best_ind_list, best_of_run_gen, \
             mean_train_fit_list, mean_test_fit_list, \
-            iodc_list, mean_iodc_list, iodc_distribution_list, \
                  slope_list, mean_slope_list, slope_distribution_list, \
-                     size_list, mean_size_list, size_distribution_list, \
-                         no_list, mean_no_list, no_distribution_list, \
-                            feats_list, mean_feats_list, feats_distribution_list = \
-            evolve(train_dataset, test_dataset, train_target, test_target, terminals)
+                        slope_augmented_list, mean_slope_augmented_list, slope_augmented_distribution_list, \
+                            size_list, mean_size_list, size_distribution_list, \
+                                no_list, mean_no_list, no_distribution_list, \
+                                    feats_list, mean_feats_list, feats_distribution_list = \
+            evolve(train_dataset, test_dataset, augmented_dataset, train_target, test_target, terminals)
         
         train_fit = pd.DataFrame([best_train_fit_list], columns = [i for i in range(0, GENERATIONS + 1)])
         test_fit = pd.DataFrame([best_test_fit_list], columns = [i for i in range(0, GENERATIONS + 1)])
@@ -44,12 +44,12 @@ def run_stdGP(ds_name):
         best_gen = pd.DataFrame([best_of_run_gen], columns = ['Gen_Number'])
         mean_train_fit = pd.DataFrame([mean_train_fit_list], columns = [i for i in range(0, GENERATIONS + 1)])
         mean_test_fit = pd.DataFrame([mean_test_fit_list], columns = [i for i in range(0, GENERATIONS + 1)])
-        iodc = pd.DataFrame([iodc_list], columns = [i for i in range(0, GENERATIONS + 1)])
-        mean_iodc = pd.DataFrame([mean_iodc_list], columns = [i for i in range(0, GENERATIONS + 1)])
-        iodc_distribution = pd.DataFrame([iodc_distribution_list], columns = [i for i in range(0, GENERATIONS + 1)])
         slope = pd.DataFrame([slope_list], columns = [i for i in range(0, GENERATIONS + 1)])
         mean_slope = pd.DataFrame([mean_slope_list], columns = [i for i in range(0, GENERATIONS + 1)])
         slope_distribution = pd.DataFrame([slope_distribution_list], columns = [i for i in range(0, GENERATIONS + 1)])
+        slope_augmented = pd.DataFrame([slope_augmented_list], columns = [i for i in range(0, GENERATIONS + 1)])
+        mean_slope_augmented = pd.DataFrame([mean_slope_augmented_list], columns = [i for i in range(0, GENERATIONS + 1)])
+        slope_augmented_distribution = pd.DataFrame([slope_augmented_distribution_list], columns = [i for i in range(0, GENERATIONS + 1)])
         size = pd.DataFrame([size_list], columns = [i for i in range(0, GENERATIONS + 1)])
         mean_size = pd.DataFrame([mean_size_list], columns = [i for i in range(0, GENERATIONS + 1)])
         size_distribution = pd.DataFrame([size_distribution_list], columns = [i for i in range(0, GENERATIONS + 1)])
@@ -67,12 +67,12 @@ def run_stdGP(ds_name):
         best_gen.to_csv(SAVE_PATH + f'best_gen_run{run_nr}.csv')
         mean_train_fit.to_csv(SAVE_PATH + f'mean_train_run{run_nr}.csv')
         mean_test_fit.to_csv(SAVE_PATH + f'mean_test_run{run_nr}.csv')
-        iodc.to_csv(SAVE_PATH + f'iodc_complexity_run{run_nr}.csv')
-        mean_iodc.to_csv(SAVE_PATH + f'mean_iodc_complexity_run{run_nr}.csv')
-        iodc_distribution.to_csv(SAVE_PATH + f'iodc_distribution_run{run_nr}.csv')
         slope.to_csv(SAVE_PATH + f'slope_complexity_run{run_nr}.csv')
         mean_slope.to_csv(SAVE_PATH + f'mean_slope_complexity_run{run_nr}.csv')
         slope_distribution.to_csv(SAVE_PATH + f'slope_distribution_run{run_nr}.csv')
+        slope_augmented.to_csv(SAVE_PATH + f'slope_augmented_complexity_run{run_nr}.csv')
+        mean_slope_augmented.to_csv(SAVE_PATH + f'mean_slope_augmented_complexity_run{run_nr}.csv')
+        slope_augmented_distribution.to_csv(SAVE_PATH + f'slope_augmented_distribution_run{run_nr}.csv')
         size.to_csv(SAVE_PATH + f'size_run{run_nr}.csv')
         mean_size.to_csv(SAVE_PATH + f'mean_size_run{run_nr}.csv')
         size_distribution.to_csv(SAVE_PATH + f'size_distribution_run{run_nr}.csv')
@@ -92,6 +92,6 @@ def run_StdGP_all_ds():
         print(f'-------------------------------- DATASET {dataset} --------------------------------')
         run_stdGP(dataset)
 
-run_stdGP('Concrete')
+run_stdGP('Istanbul')
 
 # run_StdGP_all_ds()
