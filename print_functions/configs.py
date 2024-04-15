e6dd4a8ac9a34e743544b16b3da4cf0e27b7715a
Inês Magessi <inesmagessi1011@gmail.com>
2024-04-15 17:15:16 +0100

    parser

---
+++ print_functions/configs.py
@@ -0,0 +1,9 @@
+POP_SIZE            = 100   # population size
+MIN_DEPTH           = 1    # minimal initial random tree depth
+MAX_INITIAL_DEPTH   = 7    # maximal initial random tree depth
+MAX_DEPTH           = 17
+GENERATIONS         = 500  # maximal number of generations to run evolution
+TOURNAMENT_SIZE     = 4    # size of tournament for tournament selection
+XO_RATE             = 0.9  # crossover rate 
+PROB_MUTATION       = 0.1  # per-node mutation probability
+FITNESS             = 'RMSE'
